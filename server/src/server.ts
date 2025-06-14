104fa69d5c0a47ee4d36f91395474a701b7d05f5
Daniel Levi <daniellevi@email.com>
2025-06-14 11:23:53 -0300

    chore: add setup server

---
+++ server/src/server.ts
@@ -0,0 +1,44 @@
+import fastifyCors from '@fastify/cors'
+import fastifySwagger from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
+import fastify from 'fastify'
+import {
+  hasZodFastifySchemaValidationErrors,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+
+
+export const server = fastify()
+
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+server.register(fastifyCors, { origin: '*' })
+
+server.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Upload Image API',
+      description: 'API for managing images',
+      version: '1.0.0',
+    },
+  },
+})
+server.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation Error', issues: error.validation })
+  }
+
+  console.log(error)
+  return reply.status(500).send({ message: 'Internal Server Error' })
+})
+server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
+  console.log(`HTTP Server Running 3333!`)
+})
