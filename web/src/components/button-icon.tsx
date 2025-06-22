abfc8285f9d76bdce2da9123c9841a525c12d2bf
Daniel Levi <daniellevi@email.com>
2025-06-22 08:59:17 -0300

    feat: add new button icon

---
+++ web/src/components/button-icon.tsx
@@ -0,0 +1,15 @@
+import { CopyIcon } from "@phosphor-icons/react"
+import type { ComponentProps } from "react"
+
+interface ButtonProps extends ComponentProps<'button'> {}
+
+export function ButtonIcon({ ...props}: ButtonProps) {
+  return (
+    <button 
+      className="flex items-center justify-center p-2 rounded-lg bg-grayscale-200 hover:outline-2 hover:outline-blue-base shadow-custom"
+      {...props}
+    >
+      <CopyIcon size={16} />
+    </button>
+  )
+}
--- web/src/pages/home.tsx
+++ web/src/pages/home.tsx
@@ -1,9 +1,10 @@
+import { ButtonIcon } from "@/components/button-icon";
 
 
 export function Home() {
   return(
     <div className="p-8 space-y-6 max-w-md">
-    
+      <ButtonIcon />
     </div>
   )
 }
